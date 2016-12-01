f1a6d6aa8834efaba57f2d79d27153a5c1459fe6
Brit Butler <redline6561@gmail.com>
2016-12-01 14:39:25 -0500

    Fix table name.

--- database/migrations/1480620547160_add_admin_to_users.js
+++ database/migrations/1480620547160_add_admin_to_users.js
@@ -5,13 +5,13 @@ const Schema = use('Schema')
 class AddAdminToUsersTableSchema extends Schema {
 
   up () {
-    this.table('add_admin_to_users', (table) => {
+    this.table('users', (table) => {
       table.boolean('admin').default('false').notNull()
     })
   }
 
   down () {
-    this.table('add_admin_to_users', (table) => {
+    this.table('users', (table) => {
       table.dropColumn('admin')
     })
   }
